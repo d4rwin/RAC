4ea9c95564b0c3eb3b3ffd7c26fe9e5862967f4b
D4rwin <leotimsit@gmail.com>
2016-11-10 12:03:17 +0100

    strrev

--- JOUR03/ft_strrev.c
+++ JOUR03/ft_strrev.c
@@ -1,37 +1,57 @@
 #include <unistd.h>
-void	ft_putchar(char c)
+#include <stdio.h>
+// void	ft_putchar(char c)
+// {
+// 	write(1, &c, 1);
+// }
+
+// char	*ft_strrev(char *str)
+// {
+// 	int		a;
+// 	int		b;
+// 	char	c;
+
+// 	b = 0;
+// 	while (str[b] != '\0')
+// 		b++;
+// 	a = -1;
+// 	while (a++ < b--)
+// 	{
+// 		c = str[a];
+// 		str[a] = str[b];
+// 		str[b] = c;
+// 	}
+// 	return (str);
+// }
+
+
+char    *ft_strrev(char *str)
 {
-	write(1, &c, 1);
+        int i;
+        int j;
+        char tmp;
+ 
+        i = 0;
+        j = 0;
+        while (str[i] != '\0')
+                i++;
+        i = i - 1;
+        // while (j < i/2)
+        while (i > j)	
+        {
+                tmp = str[i];
+                str[i] = str[j];
+                str[j] = tmp;
+                i--;
+                j++;
+        }
+        return (str);
 }
 
-char	*ft_strrev(char *str)
+int     main()
 {
-	int		a;
-	int		b;
-	char	c;
-
-	b = 0;
-	while (str[b] != '\0')
-		b++;
-	a = -1;
-	while (a++ < b--)
-	{
-		c = str[a];
-		str[a] = str[b];
-		str[b] = c;
-	}
-	return (str);
-}
-
-
-int main ()
-
-{
-	char str[10] = "HELLO";
-
-	ft_strrev(str);
-
-	ft_putchar(str);
-
-	return 0;
+        char    str1[] = "Hello World !";
+ 
+        printf("%s\n", ft_strrev(str1));
+        return (0);
 }

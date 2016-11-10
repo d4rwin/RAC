885e06e2f63764b897dbb112adde1be718ca7b35
D4rwin <leotimsit@gmail.com>
2016-11-10 11:54:07 +0100

    strrev

--- JOUR03/ft_strrev.c
+++ JOUR03/ft_strrev.c
@@ -1,4 +1,8 @@
 #include <unistd.h>
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 char	*ft_strrev(char *str)
 {
@@ -10,11 +14,24 @@ char	*ft_strrev(char *str)
 	while (str[b] != '\0')
 		b++;
 	a = -1;
-	while (++a < --b)
+	while (a++ < b--)
 	{
 		c = str[a];
 		str[a] = str[b];
 		str[b] = c;
 	}
 	return (str);
+}
+
+
+int main ()
+
+{
+	char str[10] = "HELLO";
+
+	ft_strrev(str);
+
+	ft_putchar(str);
+
+	return 0;
 }

44db8454238f820cc62d9f4f6d066783e8ade2ce
D4rwin <leotimsit@gmail.com>
2016-11-10 14:43:23 +0100

    ft_strlen.c

--- JOUR03/ft_strlen.c
+++ JOUR03/ft_strlen.c
@@ -1,17 +1,35 @@
 #include <unistd.h>
 
-void	ft_strlen(char *str)
+void	ft_putchar(char c)
 {
-	int count;
+	write (1, &c, 1);
+}
+
+int		ft_strlen(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (str[i])
+	{
+		i++;
+		j++;
+	}
+	return (j);
+}
+
+int main()
+{
+	char str[] = {'1', '2', '3', '4', '5', '\0'};
 	int i;
 
 	i = 0;
-	count = 0;
-	while (*str[i] != '\0')
-	{
-		ft_putchar(*str[i]);
-		i++;
-		count++;
-	}
-	return (count);
+	// ft_putstr(str);
+	i = ft_strlen(str);
+	ft_putchar(i +'0');
+	ft_putchar('\n');
+	return 0;
 }

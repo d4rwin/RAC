a936f5b114d8019c6bb633b527748cd3a976c2c5
D4rwin <leotimsit@gmail.com>
2016-12-01 15:27:55 +0100

    Jour 06

--- JOUR06/BISlibft.h
+++ JOUR06/BISlibft.h
@@ -1,9 +1,14 @@
 #ifndef __FT_H
 # define __FT_H
 
+#include <unistd.h>
+#include <string.h>
+
+
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 int		ft_strcmp(char *s1, char *s2);
 int		ft_strlen(char *str);
 void	ft_swap(int *a, int *b);
+
 #endif
